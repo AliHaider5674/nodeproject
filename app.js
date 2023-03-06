7e13d01a3ce84547241cff87cdb273907a6f61f1
AliHaider5674 <106062419+AliHaider5674@users.noreply.github.com>
2023-03-06 15:34:36 +0500

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/s', (req, res) => {
-  res.send('Hello World to this team ')
+  res.send('Hello World to this  ')
 })
 
 app.listen(port, () => {
